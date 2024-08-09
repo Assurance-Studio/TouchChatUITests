55b59e2a2161679f88e8c09cc7dea53eea0f7886
Alin Voinescu <alin.voinescu@outlook.com>
2024-08-09 17:24:00 +0300

    new speechDisplayBar test

---
+++ Pages/SpeechDisplayBarPage.swift
@@ -0,0 +1,9 @@
+//
+//  SpeechDisplayBarPage.swift
+//  TouchChatUITests
+//
+//  Created by Alin Voinescu on 09.08.2024.
+//  Copyright © 2024 PRC-Saltillo. All rights reserved.
+//
+
+import Foundation
