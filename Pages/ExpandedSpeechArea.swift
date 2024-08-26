5fa7083c43dfe43ada906d85f64bea51185f5562
Alin Voinescu <alin.voinescu@outlook.com>
2024-08-26 16:23:36 +0300

    new test and new page object

---
+++ Pages/ExpandedSpeechArea.swift
@@ -0,0 +1,9 @@
+//
+//  ExpandedSpeechArea.swift
+//  TouchChatUITests
+//
+//  Created by Alin Voinescu on 26.08.2024.
+//  Copyright © 2024 PRC-Saltillo. All rights reserved.
+//
+
+import Foundation
