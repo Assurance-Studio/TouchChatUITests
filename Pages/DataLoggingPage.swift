f6097eabdc221b026f6115231f6faae77e4ead6e
Alin Voinescu <alin.voinescu@outlook.com>
2024-08-28 11:37:15 +0300

    data logging test

---
+++ Pages/DataLoggingPage.swift
@@ -0,0 +1,9 @@
+//
+//  DataLoggingPage.swift
+//  TouchChatUITests
+//
+//  Created by Alin Voinescu on 28.08.2024.
+//  Copyright © 2024 PRC-Saltillo. All rights reserved.
+//
+
+import Foundation
