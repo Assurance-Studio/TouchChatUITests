09a48694a63c541c0800444e1f8fe902130ccfe8
Alin Voinescu <alin.voinescu@outlook.com>
2024-08-28 11:37:18 +0300

    data logging test

--- Pages/DataLoggingPage.swift
+++ Pages/DataLoggingPage.swift
@@ -1,9 +1,65 @@
-//
 //  DataLoggingPage.swift
 //  TouchChatUITests
 //
 //  Created by Alin Voinescu on 28.08.2024.
 //  Copyright © 2024 PRC-Saltillo. All rights reserved.
-//
 
 import Foundation
+import XCTest
+
+class DataLoggingPage {
+    
+    let app: XCUIApplication
+    init(app: XCUIApplication) {
+        self.app = app
+    }
+    
+    func checkLoggingImage(){
+        let enableLoggingSwitch = app.switches.element(boundBy: 21)
+        enableLoggingSwitch.tap()
+        let enableLogging = app.images["lam_indicator"]
+        let existsEnableLogging = enableLogging.waitForExistence(timeout: 5)
+        XCTAssertTrue(existsEnableLogging, "The Enable Logging image is not visible")
+        enableLoggingSwitch.tap()
+        sleep(3)
+        XCTAssertFalse(enableLogging.exists, "The Enable Logging image is still visible")
+    }
+    
+    func setPrivacyPass(){
+        let privacyPass = app.secureTextFields.element(boundBy: 1)
+        privacyPass.tap()
+        privacyPass.typeText("Test")
+        
+        let tablesQuery = XCUIApplication().popovers.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Realize Language™ Setup"]/*[[".cells.staticTexts[\"Realize Language™ Setup\"]",".staticTexts[\"Realize Language™ Setup\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(app.staticTexts["Confirm New Privacy Password"].exists, "The privacy password modal doesn't appear")
+        app.secureTextFields.element(boundBy: 1).typeText("Test")
+        app.buttons["Enter"].tap()
+        
+        XCTAssertTrue(app.staticTexts["New Password Set"].exists, "The password was not set")
+        app.buttons["Okay"].tap()
+        
+        let popoversQuery = XCUIApplication().popovers
+        popoversQuery.navigationBars["RealizeSettingsView"].buttons["Cancel"].tap()
+        popoversQuery.tables/*@START_MENU_TOKEN@*/.buttons["Clear"]/*[[".cells.buttons[\"Clear\"]",".buttons[\"Clear\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(app.staticTexts["Do you really want to Clear Log?"].exists, "The Clear Log modal doesn't appear")
+        app.buttons["Yes"].tap()
+        XCTAssertTrue(app.staticTexts["Clear Log Completed"].exists, "The Clear Log is incomplete")
+        app.buttons["Okay"].tap()
+        app.buttons["Done"].tap()
+    }
+    
+    func removePrivacyPass(){
+        let privacyPass = app.secureTextFields.element(boundBy: 1)
+        XCUIApplication().popovers.tables.staticTexts["Speech"].swipeUp()
+        XCUIApplication().popovers.tables.staticTexts["Speech Display Bar"].swipeUp()
+        XCUIApplication().popovers.tables.staticTexts["Expanded Speech Area"].swipeUp()
+        privacyPass.tap()
+        app.secureTextFields.element(boundBy: 2).typeText("Test")
+        app.buttons["Enter"].tap()
+        
+        let popoversQuery = XCUIApplication().popovers
+        popoversQuery.tables/*@START_MENU_TOKEN@*/.buttons["Clear text"]/*[[".cells",".secureTextFields.buttons[\"Clear text\"]",".buttons[\"Clear text\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        popoversQuery.navigationBars["Settings"].buttons["Done"].tap()
+    }
+}
--- Tests/SettingsFunctionality/dataLoggingTests.swift
+++ Tests/SettingsFunctionality/dataLoggingTests.swift
@@ -1,42 +1,70 @@
-//
-//  dataLoggingTests.swift
 //  TouchChatUITests
 //
 //  Created by Alin Voinescu on 27.08.2024.
 //  Copyright © 2024 PRC-Saltillo. All rights reserved.
-//
+
 
 import XCTest
 
 final class dataLoggingTests: XCTestCase {
 
+    var app = XCUIApplication()
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        
+        let pages = Pages(app: app)
+        
+        app = XCUIApplication()
+        app.launchArguments.append("--reset")
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        pages.clearAppCache()
+        pages.resetPersistentStorage()
+        pages.reachMenuPageIfOnVocabPage()
+        pages.deleteVocabFromVocabPageIfExisting(deleteCircle: "minus.circle.fill", maxScrolls: 3, timeout: 5)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    override func tearDownWithError() throws {
+        app.terminate()
+        try super.tearDownWithError()
+    }
+    
+    func testDataLoggingTests() throws {
+        let vocabularyName = "copied vocabulary data logging"
+        let vocabylaryDesc = "vocabulary description e2e"
+        var vocabName = "vocabulary"
+        lazy var mainPage: MainPage = {
+            return MainPage(app: XCUIApplication(), vocabName: vocabName)
+        }()
+        let pages = Pages(app: app)
+        let profilesAndEditingPage = ProfilesAndEditingPage(app: app)
+        let dataLoginPage = DataLoggingPage(app: app)
+        
+        pages.scrollDownUntilElementIsVisible(element: pages.basic4SS)
+        //copy a new vocab
+        mainPage.copySpellingVocab(vocabName: vocabularyName, vocabDescription: vocabylaryDesc)
+        mainPage.openVocab(vocabToOpen: app.staticTexts["copied vocabulary data logging"], vocab: vocabularyName)
+        
+        //open The Settings Menu
+        profilesAndEditingPage.openTheSettingsTab()
+        
+        //check if the enable Logging image appears
+        dataLoginPage.checkLoggingImage()
+        
+        //set privacy password and clear log
+        dataLoginPage.setPrivacyPass()
+        
+        //remove the privacy password
+        profilesAndEditingPage.openTheSettingsTab()
+        dataLoginPage.removePrivacyPass()
+                        
+        pages.backToVocab()
+        mainPage.deleteVocabFromMainPage(vocabDesc: vocabularyName)
+        
+        print("Data Logging Test Finished with success!")
+        
+        app.terminate()
+        
     }
 }
