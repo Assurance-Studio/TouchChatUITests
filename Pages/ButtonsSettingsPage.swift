31bd24f06a68ce6fcbb1eebedc5c4f4a2d5ceea3
Alin Voinescu <alin.voinescu@outlook.com>
2024-08-30 15:32:36 +0300

    new buttons test

---
+++ Pages/ButtonsSettingsPage.swift
@@ -0,0 +1,9 @@
+//
+//  ButtonsSettingsPage.swift
+//  TouchChatUITests
+//
+//  Created by Alin Voinescu on 30.08.2024.
+//  Copyright © 2024 PRC-Saltillo. All rights reserved.
+//
+
+import Foundation
