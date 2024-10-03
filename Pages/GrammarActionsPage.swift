62ea73de5b5336ae10a5cf494a16a81f0bba82df
Alin Voinescu <alin.voinescu@outlook.com>
2024-10-03 11:34:21 +0300

    new tests - grammar actions & help signal

--- Pages/GrammarActionsPage.swift
+++ Pages/GrammarActionsPage.swift
@@ -7,3 +7,151 @@
 //
 
 import Foundation
+import XCTest
+
+class GrammarActionaPageClass {
+    
+    
+    let app: XCUIApplication
+    init(app: XCUIApplication) {
+        self.app = app
+    }
+    let actionsPage = ActionaPageClass(app: XCUIApplication())
+    
+    func addActionED(){
+        app.staticTexts["add -ed"].tap()
+        app.buttons["Save"].tap()
+    }
+    
+    func addActionEN(){
+        app.buttons.element(boundBy: 26).tap()
+        app.buttons["Edit This Button"].tap()
+        sleep(2)
+        app.textFields.element(boundBy: 0).doubleTap()
+        app.textFields.element(boundBy: 0).typeText("Grammar Action -en")
+        actionsPage.removeSpeechMessageAction()
+        actionsPage.addANewAction(actionName: "Grammar Action")
+        app.staticTexts["add -en"].tap()
+        app.buttons["Save"].tap()
+    }
+    
+    func addActionER(){
+        app.buttons.element(boundBy: 27).tap()
+        app.buttons["Edit This Button"].tap()
+        sleep(2)
+        app.textFields.element(boundBy: 0).doubleTap()
+        app.textFields.element(boundBy: 0).typeText("Grammar Action -er")
+        actionsPage.removeSpeechMessageAction()
+        actionsPage.addANewAction(actionName: "Grammar Action")
+        app.staticTexts["add -er"].tap()
+        app.buttons["Save"].tap()
+    }
+    
+    func addActionEST(){
+        app.buttons.element(boundBy: 28).tap()
+        app.buttons["Edit This Button"].tap()
+        sleep(2)
+        app.textFields.element(boundBy: 0).doubleTap()
+        app.textFields.element(boundBy: 0).typeText("Grammar Action -est")
+        actionsPage.removeSpeechMessageAction()
+        actionsPage.addANewAction(actionName: "Grammar Action")
+        app.staticTexts["add -est"].tap()
+        app.buttons["Save"].tap()
+    }
+    
+    func addActionING(){
+        app.buttons.element(boundBy: 29).tap()
+        app.buttons["Edit This Button"].tap()
+        sleep(2)
+        app.textFields.element(boundBy: 0).doubleTap()
+        app.textFields.element(boundBy: 0).typeText("Grammar Action -ing")
+        actionsPage.removeSpeechMessageAction()
+        actionsPage.addANewAction(actionName: "Grammar Action")
+        app.staticTexts["add -ing"].tap()
+        app.buttons["Save"].tap()
+    }
+    
+    func addActionLY(){
+        app.buttons.element(boundBy: 30).tap()
+        app.buttons["Edit This Button"].tap()
+        sleep(2)
+        app.textFields.element(boundBy: 0).doubleTap()
+        app.textFields.element(boundBy: 0).typeText("Grammar Action -ly")
+        actionsPage.removeSpeechMessageAction()
+        actionsPage.addANewAction(actionName: "Grammar Action")
+        app.staticTexts["add -ly"].tap()
+        app.buttons["Save"].tap()
+    }
+    
+    func addActionS(){
+        app.buttons.element(boundBy: 31).tap()
+        app.buttons["Edit This Button"].tap()
+        sleep(2)
+        app.textFields.element(boundBy: 0).doubleTap()
+        app.textFields.element(boundBy: 0).typeText("Grammar Action -s")
+        actionsPage.removeSpeechMessageAction()
+        actionsPage.addANewAction(actionName: "Grammar Action")
+        app.staticTexts["add -s"].tap()
+        app.buttons["Save"].tap()
+    }
+    
+    func createAVerb(){
+        app.buttons["z"].tap()
+        app.buttons["Edit This Button"].tap()
+        sleep(2)
+        app.textFields.element(boundBy: 0).doubleTap()
+        app.textFields.element(boundBy: 0).typeText("break")
+        app.buttons["Save"].tap()
+    }
+    
+    func createAnAdjective(){
+        app.buttons["x"].tap()
+        app.buttons["Edit This Button"].tap()
+        sleep(2)
+        app.textFields.element(boundBy: 0).doubleTap()
+        app.textFields.element(boundBy: 0).typeText("quick")
+        app.buttons["Save"].tap()
+        app.navigationBars["SPKBD-QWERTY"].buttons["Done"].tap()
+    }
+    
+    func checkGrammarAction(){
+        //check if the grammar action -ed works as expected
+        app.buttons["break"].tap()
+        app.buttons["Grammar Action -ed"].tap()
+        app.buttons["g"].tap()
+        app.buttons["BackButton"].tap()
+        //check if the grammar action -en works as expected
+        app.buttons["break"].tap()
+        app.buttons["Grammar Action -en"].tap()
+        app.buttons["g"].tap()
+        app.buttons["BackButton"].tap()
+        //check if the grammar action -er works as expected
+        app.buttons["break"].tap()
+        app.buttons["Grammar Action -er"].tap()
+        app.buttons["g"].tap()
+        app.buttons["BackButton"].tap()
+        //check if the grammar action -est works as expected
+        app.buttons["quick"].tap()
+        app.buttons["Grammar Action -est"].tap()
+        app.buttons["g"].tap()
+        app.buttons["BackButton"].tap()
+        //check if the grammar action -ing works as expected
+        app.buttons["break"].tap()
+        app.buttons["Grammar Action -ing"].tap()
+        app.buttons["g"].tap()
+        app.buttons["BackButton"].tap()
+        //check if the grammar action -ly works as expected
+        app.buttons["quick"].tap()
+        app.buttons["Grammar Action -ly"].tap()
+        app.buttons["g"].tap()
+        app.buttons["BackButton"].tap()
+        //check if the grammar action -s works as expected
+        app.buttons["break"].tap()
+        app.buttons["Grammar Action -s"].tap()
+        app.buttons["g"].tap()
+        app.buttons["BackButton"].tap()
+        
+        let grammarActions = app.textFields["Broke broken breaker quickest breaking quickly breaks "]
+        XCTAssertTrue(grammarActions.exists, "The grammar actions don't work as expected")
+    }
+}
--- Tests/ButtonActions/grammarActionTests.swift
+++ Tests/ButtonActions/grammarActionTests.swift
@@ -1,5 +1,3 @@
-//
-//  grammarActionTests.swift
 //  TouchChatUITests
 //
 //  Created by Alin Voinescu on 02.10.2024.
@@ -10,33 +8,84 @@ import XCTest
 
 final class grammarActionTests: XCTestCase {
 
+    var app = XCUIApplication()
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        
+        let pages = Pages(app: app)
+        
+        app = XCUIApplication()
+        app.launchArguments.append("--reset-app-state")
         app.launch()
+        pages.clickWelcomeX()
+        pages.reachMenuPageIfOnVocabPage()
+   }
+   
+   override func tearDownWithError() throws {
+       app.terminate()
+       try super.tearDownWithError()
+   }
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+   func testGrammarAction() throws {
+       
+       let pages = Pages(app: app)
+       let actionsPage = ActionaPageClass(app: app)
+       let grammarActionsPage = GrammarActionaPageClass(app: app)
+       let vocabularyName = "copied vocabulary grammar action"
+       let vocabylaryDesc = "vocabulary description e2e"
+       var vocabName = "vocabulary"
+       lazy var mainPage: MainPage = {
+           return MainPage(app: XCUIApplication(), vocabName: vocabName)
+       }()
+       
+       //copy a new vocab
+       mainPage.copySpellingVocab(vocabName: vocabularyName, vocabDescription: vocabylaryDesc)
+       sleep(2)
+       mainPage.openVocab(vocabToOpen: app.staticTexts["copied vocabulary grammar action"], vocab: vocabularyName)
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+       pages.editPage()
+       //add an action
+       actionsPage.editButtonForAction(nameButton: "Grammar Action -ed")
+       
+       //remove the speech message action
+       actionsPage.removeSpeechMessageAction()
+       
+       //add grammar action -ed
+       actionsPage.addANewAction(actionName: "Grammar Action")
+       grammarActionsPage.addActionED()
+       
+       //add grammar action -en
+       grammarActionsPage.addActionEN()
+       
+       //add grammar action -er
+       grammarActionsPage.addActionER()
+       
+       //add grammar action -est
+       grammarActionsPage.addActionEST()
+       
+       //add grammar action -ing
+       grammarActionsPage.addActionING()
+       
+       //add grammar action -ly
+       grammarActionsPage.addActionLY()
+       
+       //add grammar -s
+       grammarActionsPage.addActionS()
+       
+       //create a new button
+       grammarActionsPage.createAVerb()
+       
+       //app.buttons["z"].tap()
+       grammarActionsPage.createAnAdjective()
+       
+       //check if the grammar action works as expected
+       grammarActionsPage.checkGrammarAction()
+       
+       pages.backToVocab()
+       mainPage.deleteVocabFromMainPage(vocabDesc: vocabularyName)
+       
+       print("Grammar Action Test Finished with success!")
     }
 }
