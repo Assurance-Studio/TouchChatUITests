853cbeabf50aa73ed0d3fd9b9aa66240522ab5b5
alinvoinescuTestTableta <alin.voinescu@outlook.com>
2025-03-27 16:17:34 +0200

    new fixes and new tests

---
+++ Pages/LanguagesRegionPage.swift
@@ -0,0 +1,9 @@
+//
+//  LanguagesRegionPage.swift
+//  TouchChatAppUITests
+//
+//  Created by Alin Voinescu on 12.03.2025.
+//  Copyright © 2025 PRC-Saltillo. All rights reserved.
+//
+
+import Foundation
---
+++ Pages/LicenseLanguagePage.swift
@@ -0,0 +1,9 @@
+//
+//  LicenseLanguagePage.swift
+//  TouchChatAppUITests
+//
+//  Created by Alin Voinescu on 10.03.2025.
+//  Copyright © 2025 PRC-Saltillo. All rights reserved.
+//
+
+import Foundation
